a503e692e511a40cb5d258c5e0ca4f89f8029ac0
Guillian J <guillian19992511@gmail.com>
2022-08-20 16:35:30 +0200

    fix jackhammer

--- supcom2_cards/Cards/Jackhammer.cs
+++ supcom2_cards/Cards/Jackhammer.cs
@@ -11,15 +11,14 @@ namespace Supcom2Cards.Cards
 {
     class Jackhammer : CustomCard
     {
-        private RemoveAfterSeconds? explosionRemove;
-        private ObjectsToSpawn? explosionSpawn;
+        private readonly ObjectsToSpawn[] explosionToSpawn = new ObjectsToSpawn[1];
 
         public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers, Block block)
         {
             UnityEngine.Debug.Log($"[{Supcom2.ModInitials}][Card] {GetTitle()} has been setup.");
             //Edits values on card itself, which are then applied to the player in `ApplyCardStats`
 
-            cardInfo.allowMultiple = false; // TODO: Make projectile bigger on multiple cards or something, rn it doesn't do anything
+
         }
         public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
         {
@@ -30,40 +29,41 @@ namespace Supcom2Cards.Cards
             gun.projectileSize += 3f;
             
             gun.projectileSpeed *= 1.5f;
-            //gun.attackSpeed /= 0.2f;
+            gun.attackSpeed /= 0.2f;
 
             // add explosion effect
-            if (explosionSpawn == null)
+            if (explosionToSpawn[0] == null)
             {
+                // load explosion effect from Explosive Bullet card
                 GameObject? explosiveBullet = (GameObject)Resources.Load("0 cards/Explosive bullet");
-                GameObject explosion = Instantiate(explosiveBullet.GetComponent<Gun>().objectsToSpawn[0].effect);
-                explosion.transform.position = new Vector3(1000, 0, 0);
-                explosion.hideFlags = HideFlags.HideAndDontSave;
-                explosionRemove = explosion.GetComponent<RemoveAfterSeconds>();
-                explosionRemove.seconds = 1000000f;
-                explosion.GetComponent<Explosion>().force = 100000;
+                GameObject A_ExplosionSpark = explosiveBullet.GetComponent<Gun>().objectsToSpawn[0].AddToProjectile;
+                GameObject A_Explosion = explosiveBullet.GetComponent<Gun>().objectsToSpawn[0].effect;
 
-                explosionSpawn = new ObjectsToSpawn
+                explosionToSpawn[0] = new ObjectsToSpawn
                 {
-                    effect = explosion,
+                    AddToProjectile = A_ExplosionSpark,
+                    direction = ObjectsToSpawn.Direction.forward,
+                    effect = A_Explosion,
                     normalOffset = 0.1f,
-                    numberOfSpawns = 1,
-                    scaleFromDamage = .5f,
+                    scaleFromDamage = 0.5f,
+                    scaleStackM = 0.7f,
+                    scaleStacks = true,
+                    spawnAsChild = false,
+                    spawnOn = ObjectsToSpawn.SpawnOn.all,
+                    stacks = 0,
+                    stickToAllTargets = false,
+                    stickToBigTargets = false,
+                    zeroZ = false
                 };
             }
-            gun.objectsToSpawn = new[] { explosionSpawn };
+            gun.objectsToSpawn = gun.objectsToSpawn.Concat(explosionToSpawn).ToArray();
         }
         public override void OnRemoveCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
         {
             UnityEngine.Debug.Log($"[{Supcom2.ModInitials}][Card] {GetTitle()} has been removed from player {player.playerID}.");
             //Run when the card is removed from the player
 
-            // remove explosion effect
-            if (explosionRemove != null)
-            {
-                explosionRemove.seconds = 0f;
-                explosionSpawn = null;
-            }
+            gun.objectsToSpawn = gun.objectsToSpawn.Except(explosionToSpawn).ToArray();
         }
 
         protected override string GetTitle()
