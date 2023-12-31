6aeb4f59de36526aa1495be3cc0b858e0a59f08d
Guillian J <guillian19992511@gmail.com>
2023-12-31 01:19:35 +0100

    update SuperTriton

--- supcom2_cards/Cards/SuperTriton.cs
+++ supcom2_cards/Cards/SuperTriton.cs
@@ -62,7 +62,7 @@ namespace Supcom2Cards.Cards
                 {
                     positive = false,
                     stat = "ATKSPD",
-                    amount = "50%",
+                    amount = "-100%",
                     simepleAmount = CardInfoStat.SimpleAmount.notAssigned
                 },
                 new CardInfoStat()
