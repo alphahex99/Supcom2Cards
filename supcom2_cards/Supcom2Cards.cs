82842816e7a8f95b46b3b43a7921b5a5f25d2d7b
guill <->
2024-09-05 13:05:07 +0200

    v2.0.1

--- supcom2_cards/Supcom2Cards.cs
+++ supcom2_cards/Supcom2Cards.cs
@@ -19,7 +19,7 @@ namespace Supcom2Cards
     [BepInProcess("Rounds.exe")]
     public class Supcom2 : BaseUnityPlugin
     {
-        public const string Version = "2.0.0";
+        public const string Version = "2.0.1";
 
         private const string ModId = "com.alphahex.rounds.supcom2cards";
         private const string ModName = "Supcom2 Cards";
